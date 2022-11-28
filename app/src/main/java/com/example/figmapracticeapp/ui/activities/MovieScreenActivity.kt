8dd1746abf376acba600584ad4cfc097a10507be
shaham.shahzad <shaham300@gmail.com>
2022-11-28 12:33:51 +0500

    3rd commit

--- app/src/main/java/com/example/figmapracticeapp/ui/activities/MovieScreenActivity.kt
+++ app/src/main/java/com/example/figmapracticeapp/ui/activities/MovieScreenActivity.kt
@@ -1,12 +1,9 @@
 package com.example.figmapracticeapp.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.NavController.OnDestinationChangedListener
-import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
-import com.example.figmapracticeapp.R
 import com.example.figmapracticeapp.databinding.ActivityMovieScreenBinding
 import com.example.figmapracticeapp.ui.model.DotsIndicator
 
@@ -14,9 +11,10 @@ import com.example.figmapracticeapp.ui.model.DotsIndicator
 class MovieScreenActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMovieScreenBinding
-    private var navGraph : NavGraph? = null
+//    private var navGraph : NavGraph? = null
     private var navController : NavController? = null
-    fun getDotIndicator() : DotsIndicator = findViewById(R.id.dotsIndicator)
+
+    fun getDotIndicator() : DotsIndicator = binding.toolbarLayout.dotsIndicator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,39 +23,18 @@ class MovieScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-//       getDotIndicator().initDots(3)
-//        navController!!.addOnDestinationChangedListener( OnDestinationChangedListener { controller, destination, arguments ->
-//
-//            when(destination.id)
-//            {
-//                R.id.movieTicketSeats ->
-//                {
-//                    getDotIndicator().setDotSelection(2)
-//                }
-//            }
-//
-//
-//        } )
-//
-//
-
-
         binding.toolbarLayout.ivBackBtn.setOnClickListener {
             //On back arrow press move back to DashBoard Activity
-            navController =  binding.frgContainer1.getFragment<NavHostFragment>().navController
+            navController = binding.frgContainer1.getFragment<NavHostFragment>().navController
             val isStackPopped = navController!!.popBackStack() // Navigate back to the last fragment
             if (!isStackPopped) finish() // If there is no fragments in the stack finish the activity and go back to activity dashboard
-           }
+        }
         binding.toolbarLayout.ivCloseBtn.setOnClickListener {
 
             finish()
 
         }
 
-
-//        val navHost = supportFragmentManager.findFragmentById(R.id.frg_container1) as NavHostFragment?
-//        val navController = navHost?.navController
-//        navController?.setGraph(R.navigation.nav)
     }
 
 }
--- app/src/main/java/com/example/figmapracticeapp/ui/fragment/MovieReviewFragment.kt
+++ app/src/main/java/com/example/figmapracticeapp/ui/fragment/MovieReviewFragment.kt
@@ -1,15 +1,21 @@
 package com.example.figmapracticeapp.ui.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import com.example.figmapracticeapp.R
 import com.example.figmapracticeapp.databinding.ChairLayoutBinding
 import com.example.figmapracticeapp.databinding.FragmentMovieReviewBinding
 import com.example.figmapracticeapp.databinding.FragmentMovieTicketSeatsBinding
 import com.example.figmapracticeapp.ui.activities.MovieScreenActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.nio.file.attribute.AclEntry.Builder
 
 
 class MovieReviewFragment : Fragment() {
@@ -28,6 +34,29 @@ class MovieReviewFragment : Fragment() {
             initDots(4)
             setDotSelection(2)
         }
+
+
+
+
+        binding.confirmTransactionBtn.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.fingerprint_layout, null)
+            val dialog = BottomSheetDialog(requireContext(),R.style.AppBottomSheetDialogTheme2)
+
+            dialog.behavior.isDraggable = false
+            dialog.setCancelable(true)
+            dialog.setCanceledOnTouchOutside(false)
+            dialog.setContentView(view)
+            dialog.show()
+
+
+/*            val view = layoutInflater.inflate(R.layout.select_cinema_bottomsheet, null)
+            val dialog : AlertDialog.Builder = AlertDialog.Builder(requireContext(),R.style.AppBottomSheetDialogTheme)
+            val alertDialog :AlertDialog = dialog.create()
+            alertDialog.window?.setGravity(Gravity.BOTTOM)
+            alertDialog.setView(view)
+            alertDialog.show()*/
+        }
+
 
         return binding.root
     }
